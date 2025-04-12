b30b58024687dce81a18a11be5d877ec6e69421f
Haitham Said <HaithamSoliman.ext@adcb.com>
2025-04-12 17:14:37 +0400

    maintainance project structure

---
+++ maintainance/src/main/java/com/fixora/maintainance/MaintainanceApplication.java
@@ -0,0 +1,13 @@
+package com.fixora.maintainance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintainanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintainanceApplication.class, args);
+	}
+
+}
